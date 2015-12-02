f6e349b7fd67c0f730b935d512ec9b360263167c
Nicolas Fredrickson <fredricksonnnicolas@gmail.com>
2015-12-02 13:07:42 -0800

    Changed the railsPrep script.

--- railsPrep.sh
+++ railsPrep.sh
@@ -1,9 +1,15 @@
 # Use this once you've first pulled the repository;
 # It installs the gems and other things you'll need.
 
+echo "~~~~~~~ cd CI_FINDER"
 cd CI_FINDER
-rake db:create
-rake db:migrate
+echo "~~~~~~~ bundle install"
 bundle install
+echo "~~~~~~~ rails g simple_form:install --bootstrap"
 rails g simple_form:install --bootstrap
+echo "~~~~~~~ bundle install"
 bundle install
+echo "~~~~~~~ rake db:create"
+rake db:create
+echo "~~~~~~~ rake db:migrate"
+rake db:migrate
